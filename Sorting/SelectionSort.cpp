31b93a8540819c86c07794251c8029267d156429
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-14 21:31:51 +0530

    Add selection sort program

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = 5;
+
+    // Selection Sort
+    for (int i = 0; i < n - 1; i++) {
+
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    cout << "Sorted Array: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
