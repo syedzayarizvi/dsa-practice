6545c09e7af9148fbeaa97ea1fe8ce951fe0ea1a
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-14 21:41:08 +0530

    Add reverse string program

---
+++ Strings/ReverseString.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string str = "GitHub";
+
+    cout << "Original String: " << str << endl;
+
+    cout << "Reversed String: ";
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+
+    cout << endl;
+
+    return 0;
+}
