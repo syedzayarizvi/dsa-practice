05cf9847e252e5908c78eb17dd747220432d2f53
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-18 00:18:10 +0530

    Add singly linked list program

---
+++ LinkedList/SinglyLinkedList.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+int main() {
+
+    Node* head = new Node(10);
+
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+
+    Node* temp = head;
+
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
