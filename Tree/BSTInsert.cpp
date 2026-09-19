572eb512315ac4eedbb934e544d2dd24262a4d2e
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-19 20:40:38 +0530

    Add BST insertion

---
+++ Tree/BSTInsert.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int value) {
+
+    if (root == nullptr)
+        return new Node(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+void inorder(Node* root) {
+
+    if (root == nullptr)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+
+    Node* root = nullptr;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+
+    cout << "BST Inorder: ";
+    inorder(root);
+
+    return 0;
+}
