48d3b8901a415b473a79a1d1d33f01ec234f0ebd
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-19 21:22:41 +0530

    Add Fibonacci using recursion

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+
+    if (n == 0)
+        return 0;
+
+    if (n == 1)
+        return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+
+    int n = 7;
+
+    cout << "Fibonacci number at position " << n
+         << " = " << fibonacci(n);
+
+    return 0;
+}
