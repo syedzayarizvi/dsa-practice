a3e3af6a3217bbf7d4a3495e26119b676dad0552
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-20 16:17:05 +0530

    Add remove duplicates using two pointer

---
+++ TwoPointer/RemoveDuplicates.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[] = {1, 1, 2, 2, 3, 4, 4, 5};
+    int n = 8;
+
+    int j = 0;
+
+    for (int i = 1; i < n; i++) {
+
+        if (arr[i] != arr[j]) {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+
+    cout << "Array after removing duplicates: ";
+
+    for (int i = 0; i <= j; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
