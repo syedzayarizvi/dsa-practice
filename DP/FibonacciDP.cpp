f580d80b5278377b7b83de5bc3958859f8d6ca39
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-20 16:29:00 +0530

    Add Fibonacci using dynamic programming

---
+++ DP/FibonacciDP.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int n = 10;
+
+    vector<int> dp(n + 1);
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    cout << "Fibonacci number at position "
+         << n << " = " << dp[n];
+
+    return 0;
+}
